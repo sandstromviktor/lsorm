3b4ec3e0a399956bb07af02200b039b14ad7378b
sandstromviktor <v.sandstrom@icloud.com>
2024-02-12 10:53:23 +0100

    update settings

--- settings.py
+++ settings.py
@@ -1,11 +1,9 @@
 import os
 
 DB_TYPE = "mysql+pymysql"
-DB_USERNAME = "isoposso"  # os.getenv('DB_USERNAME', username)
-DB_PASSWORD = "secret"  # os.getenv('DB_PASSWORD', password)
-DB_HOST = "localhost"  # os.getenv('DB_HOST', host)
-DB_NAME = "isoposso"  # os.getenv('DB_NAME', database)
+DB_USERNAME = "some-username"  
+DB_PASSWORD = "secret"  
+DB_HOST = "localhost"  
+DB_NAME = "mydatabase"
 
-PREFIX = "iso"
-
-INIT_TYPE = "settings"
+PREFIX = "lime"
